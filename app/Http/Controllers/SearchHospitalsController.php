f9c6f98a28c29a1703061c5f5fa1f66db5895b27
MM <mmgit@mediamechanic.net>
2023-09-29 10:54:00 +0530

    29-Sep

--- app/Http/Controllers/SearchHospitalsController.php
+++ app/Http/Controllers/SearchHospitalsController.php
@@ -3,16 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\hospitals;
 
 class SearchHospitalsController extends Controller
 {
-    function searchHospitals(Request $req) {
-        $lat = $req->lat;
-        $lng = $req->lng;
-
-        $hospitals = hospitals::whereBetween( 'lat', [$lat-0.2, $lat+0.2] )->whereBetween( 'lng ', [$lng-0.2, $lng+0.2] )->get();
-
-        return $hospitals;
-    }
+    //
 }
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <p id="copyright">Copyright &copy; 2020 <a href="#" target="_blank">Hemant Dhiman</a>. All right reserved</p>
+    <p id="copyright">Copyright &copy; 2020 <a href="https://hemantt07.netlify.app" target="_blank">Hemant Dhiman</a>. All right reserved</p>
 </footer>
 
   
